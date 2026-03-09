4eb0315bf87b83abad84678dbf3944649c8bc029
Gilvan Alencar <48884274+Gil135@users.noreply.github.com>
2026-03-09 01:41:38 -0300

    Update index.js

--- app/controllers/instanceInfo/index.js
+++ app/controllers/instanceInfo/index.js
@@ -1,23 +1,29 @@
+'use strict';
+
 var metadataService = require('../../../lib/metadataService');
 
-exports.name = 'instance-info';
+// Nome e prefixo seguem o padrão do lib/boot.js
+// boot.js mapeia exports.list → GET /api/instance-infos
+exports.name   = 'instance-info';
 exports.prefix = '/api';
 
-exports.list = function(req, res) {
+// GET /api/instance-infos
+// Retorna os dados da instância EC2 que está servindo esta requisição
+exports.list = function (req, res) {
   metadataService.getInstanceMetadata()
-    .then(function(metadata) {
+    .then(function (metadata) {
       return res.status(200).json({
-        instanceId: metadata.instanceId,
-        localIp: metadata.localIp,
-        publicIp: metadata.publicIp,
-        instanceType: metadata.instanceType,
+        instanceId:       metadata.instanceId,
+        localIp:          metadata.localIp,
+        publicIp:         metadata.publicIp,
+        instanceType:     metadata.instanceType,
         availabilityZone: metadata.availabilityZone,
-        environment: metadata.environment,
-        isAWS: metadata.isAWS
+        environment:      metadata.environment,
+        isAWS:            metadata.isAWS
       });
     })
-    .catch(function(error) {
-      console.error('[InstanceInfo] Erro:', error.message);
+    .catch(function (err) {
+      console.error('[instanceInfo] Erro:', err.message);
       return res.status(500).json({ error: 'Erro ao buscar informações da instância.' });
     });
 };
