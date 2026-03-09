ced57adaa8060999f845ef4cb898b8dd711f8968
Gilvan Alencar <48884274+Gil135@users.noreply.github.com>
2026-03-09 20:47:58 -0300

    Update EC2InfoBanner.jsx

--- client/src/components/EC2InfoBanner/EC2InfoBanner.jsx
+++ client/src/components/EC2InfoBanner/EC2InfoBanner.jsx
@@ -70,7 +70,7 @@ const EC2InfoBanner = () => {
       </div>
 
       <div className={`${styles.badge} ${isEC2 ? styles.badgeEC2 : styles.badgeLocal}`}>
-        {isEC2 ?  "🟡 Local Dev" : "✅ EC2 AWS" }
+        {isEC2 ? "✅ EC2 AWS" : "🟡 Local Dev"}
       </div>
     </div>
   );
