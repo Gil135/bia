74aca01264e9889d66267a79233fa685ffe77e9d
Gilvan Alencar <48884274+Gil135@users.noreply.github.com>
2026-03-09 12:45:12 -0300

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,9 +7,9 @@ import Footer from "./components/Footer.jsx";
 import Tasks from "./components/Tasks.jsx";
 import AddTask from "./components/AddTask.jsx";
 import About from "./components/About.jsx";
-import DebugLogs from "./components/DebugLogs.jsx"; // client/src/App.jsx
+import DebugLogs from "./components/DebugLogs.jsx";
 import React from "react";
-import EC2InfoBanner from "./components/EC2InfoBanner/EC2InfoBanner"; // ... seus outros imports
+import EC2InfoBanner from "./components/EC2InfoBanner/EC2InfoBanner";
 
 function App() {
   return (
